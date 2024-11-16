d3a234830d50932a7c757c76e6550eb9e512ba66
Hongse0 <hongseyoung981007@gmail.com>
2024-11-16 20:11:12 +0900

    Feat : 카드 조회, 카드 등록

---
+++ src/main/java/com/kb/card/dto/CardDTO.java
@@ -0,0 +1,25 @@
+package com.kb.card.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CardDTO {
+    private Long cardNo;          // 카드 번호 (Primary Key)
+    private Long userNo;          // 사용자 번호 (Foreign Key)
+    private String cardName;      // 카드 이름
+    private String cardNumber;    // 카드 번호
+    private Date expiryDate;      // 유효기간
+    private String cardType;      // 카드 타입
+    private String cardCompany;   // 카드 발급사
+    private Integer billingDate;  // 청구일
+    private Timestamp registerDate; // 카드 등록일
+}
---
+++ src/main/java/com/kb/card/mapper/CardMapper.java
@@ -0,0 +1,10 @@
+package com.kb.card.mapper;
+
+import com.kb.card.dto.CardDTO;
+import java.util.List;
+
+public interface CardMapper {
+
+    List<CardDTO> findCardsByUserNo(Long userNo);
+    void insertCard(CardDTO cardDTO);
+}
